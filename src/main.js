c749d98eb11d4797aa59d451382273f272f7a92d
zorillabiz <164332283+zorillabiz@users.noreply.github.com>
2024-04-01 15:14:12 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -82,7 +82,7 @@ Apify.main(async () => {
             const linksToEnqueueOptions = {
                 page,
                 requestQueue,
-                selector: 'a',
+                selector: 'a:contains(Contact), a',
                 sameDomain,
                 urlDomain: helpers.getDomain(request.url),
                 startUrl: request.userData.startUrl,
