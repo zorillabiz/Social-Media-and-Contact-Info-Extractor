3910d78c0da11a0e92f0793c93806e2edc7b9f12
zorillabiz <164332283+zorillabiz@users.noreply.github.com>
2024-04-02 14:41:03 +0200

    Update helpers.js

--- src/helpers.js
+++ src/helpers.js
@@ -9,10 +9,9 @@ const { Request } = Apify;
 async function extractUrlsFromPage(page, selector, sameDomain, urlDomain) {
     /* istanbul ignore next */
     const regex = /(contact|kontakt)/;
-    const allLinks = await page.$$eval(selector, (linkEls) => linkEls);
+    const allLinks = await page.$$(selector);
 
     console.log(allLinks);
-
 
     const filteredLinks = allLinks.filter((url) => (sameDomain ? module.exports.getDomain(url) === urlDomain : true));
     log.info(`Found ${filteredLinks.length} links on ${page.url()}`);
