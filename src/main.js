0f2a5f62f1cf73f6006e74821d5465ccbdff920f
zorillabiz <164332283+zorillabiz@users.noreply.github.com>
2024-06-05 15:01:13 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -37,6 +37,7 @@ Apify.main(async () => {
     for await (const req of fromStartUrls(startUrls)) {
         processedStartUrls.push(req);
     }
+    //console.log(processedStartUrls);
 
     const requestQueue = await Apify.openRequestQueue();
     const requestList = await Apify.openRequestList('start-urls', normalizeUrls(processedStartUrls));
