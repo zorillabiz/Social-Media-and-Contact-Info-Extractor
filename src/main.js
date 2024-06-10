156b7705ccbc25180be23ad721c74409c930a227
zorillabiz <164332283+zorillabiz@users.noreply.github.com>
2024-06-10 13:04:04 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -100,7 +100,7 @@ Apify.main(async () => {
                 return;
             }
 
-            const blacklist = ['dan.com', 'afternic.com', 'godaddy.com', 'sedo.com', 'buydomains.com', 'hugedomains.com', 'dovendi.com', 'aftermarket.pl', 'sawbrokers.com', 'top-domains.ch'];
+            const blacklist = ['dan.com', 'afternic.com', 'godaddy.com', 'sedo.com', 'aftermarket.pl', 'buydomains.com', 'dovendi.com', 'hugedomains.com', 'nidoma.com', 'reg.ru', 'sawbrokers.com', 'timeweb.ru', 'top-domains.ch'];
             if (blacklist.includes(helpers.getDomain(page.url()))) {
                 log.info(`Skipping ${request.url} (domain blacklisted)`);
                 return;
